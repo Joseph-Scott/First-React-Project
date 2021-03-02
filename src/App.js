56c99f29ab5d98c7ca7e08204541f9218f810678
Joe Scott <jbs198585@gmail.com>
2021-03-02 16:57:01 -0500

    Passing method references between components

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const app = props => {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
       <Person 
         name={personsState.persons[0].name} 
         age={personsState.persons[0].age} 
