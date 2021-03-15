1a28a141f201b9bbe112e28ca95d3c1fe2bd6058
Joe Scott <jbs198585@gmail.com>
2021-03-15 07:01:17 -0400

    Outputting lists

--- src/App.js
+++ src/App.js
@@ -54,6 +54,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
+          {this.state.persons.map(person => {
+            
+          })}
           <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age} />
